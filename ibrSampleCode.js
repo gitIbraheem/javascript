834d4ede8ee897ca0ffe64828b90f4d6e2d1790d
Ibraheem <ibraheem@Franks-iMac.local>
2018-09-03 13:42:53 -0500

    Sep 3, 2018 - Teaching Ibraheem for-loops & if statements (ibrSampleCode.js), Ibraheem coded his own math logic (yahyaTotal.js)

---
+++ ibrSampleCode.js
@@ -0,0 +1,21 @@
+// Baba teaching Ibraheem
+// If logic
+// For Loop
+// Sept 2, 2018
+
+Ibr = 7;
+
+if (Ibr === 7) {
+    console.log('Ibr is 7');
+}
+else {
+    console.log('Ibr is not 7');
+    console.log('Ibr is ' + Ibr)
+}
+
+//----------
+// For Loop
+
+for (i = 0; i < 3; i++) {
+    console.log('i equals: ' + i);
+}
---
+++ yahyaTotal.js
@@ -0,0 +1,6 @@
+// Ibraheem Younus
+// Sept 3
+
+var yahya = 1;
+var total = yahya + yahya;
+console.log('the total: ' + total)
